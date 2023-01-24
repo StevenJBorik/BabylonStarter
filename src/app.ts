b21b36ae00ff4dd95fd16ee5a5cdb8d0d06c81b6
Steven Borik <98104308+StevenJBorik@users.noreply.github.com>
2023-01-24 09:23:15 -0600

    misc changes

--- src/app.ts
+++ src/app.ts
@@ -71,7 +71,7 @@ class App {
 
     private async _main(): Promise<void> {
         await this._goToStart();
-    
+
         // Register a render loop to repeatedly render the scene
         this._engine.runRenderLoop(() => {
             switch (this._state) {
@@ -82,6 +82,15 @@ class App {
                     this._scene.render();
                     break;
                 case State.GAME:
+                    //if 240seconds/ 4mins have have passed, go to the lose state
+                    if (this._ui.time >= 240 && !this._player.win) {
+                        this._goToLose();
+                        this._ui.stopTimer();
+                    }
+                    if (this._ui.quit) {
+                        this._goToStart();
+                        this._ui.quit = false;
+                    }
                     this._scene.render();
                     break;
                 case State.LOSE:
@@ -90,7 +99,7 @@ class App {
                 default: break;
             }
         });
-    
+
         //resize if the screen is resized/rotated
         window.addEventListener('resize', () => {
             this._engine.resize();
@@ -521,8 +530,258 @@ class App {
             this.assets = assets;
         });
     }
+    private async _initializeGameAsync(scene): Promise<void> {
 
-    private async _goToGame(): Promise<void> { }
+        scene.ambientColor = new Color3(0.34509803921568627, 0.5568627450980392, 0.8352941176470589);
+        scene.clearColor = new Color4(0.01568627450980392, 0.01568627450980392, 0.20392156862745098);
+
+        const light = new PointLight("sparklight", new Vector3(0, 0, 0), scene);
+        light.diffuse = new Color3(0.08627450980392157, 0.10980392156862745, 0.15294117647058825);
+        light.intensity = 35;
+        light.radius = 1;
+
+        const shadowGenerator = new ShadowGenerator(1024, light);
+        shadowGenerator.darkness = 0.4;
+
+        //Create the player
+        this._player = new Player(this.assets, scene, shadowGenerator, this._input);
+
+        const camera = this._player.activatePlayerCamera();
+
+        //set up lantern collision checks
+        this._environment.checkLanterns(this._player);
+
+        //--Transition post process--
+        scene.registerBeforeRender(() => {
+            if (this._ui.transition) {
+                this._ui.fadeLevel -= .05;
+
+                //once the fade transition has complete, switch scenes
+                if(this._ui.fadeLevel <= 0) {
+                    this._ui.quit = true;
+                    this._ui.transition = false;
+                }
+            }
+        })
+
+        //--GAME LOOP--
+        scene.onBeforeRenderObservable.add(() => {
+            //reset the sparkler timer
+            if (this._player.sparkReset) {
+                this._ui.startSparklerTimer(this._player.sparkler);
+                this._player.sparkReset = false;
+
+                this._ui.updateLanternCount(this._player.lanternsLit);
+            }
+            //stop the sparkler timer after 20 seconds
+            else if (this._ui.stopSpark && this._player.sparkLit) {
+                this._ui.stopSparklerTimer(this._player.sparkler);
+                this._player.sparkLit = false;
+            }
+
+            //if you've reached the destination and lit all the lanterns
+            if (this._player.win && this._player.lanternsLit == 22) {
+                this._ui.gamePaused = true; //stop the timer so that fireworks can play and player cant move around
+                //dont allow pause menu interaction
+                this._ui.pauseBtn.isHitTestVisible = false;
+
+                let i = 10; //10 seconds
+                window.setInterval(() => {
+                    i--;
+                    if (i == 0) {
+                        this._showWin();
+                    }
+                }, 1000);
+
+                this._environment._startFireworks = true;
+                this._player.win = false;
+            }
+
+            if (!this._ui.gamePaused) {
+                this._ui.updateHud();
+            }
+            //if the player has attempted all tutorial moves, move on to the hint IF they haven't already lit the next lantern
+            if(this._player.tutorial_move && this._player.tutorial_jump && this._player.tutorial_dash && (this._ui.tutorial.isVisible || this._ui.hint.isVisible)){
+                this._ui.tutorial.isVisible = false;
+                if(!this._environment._lanternObjs[1].isLit){ // if the first lantern hasn't been lit, then give hint as to which direction to go
+                    this._ui.hint.isVisible = true;
+                } else {
+                    this._ui.hint.isVisible = false;
+                }
+            }
+        })
+        //glow layer
+        const gl = new GlowLayer("glow", scene);
+        gl.intensity = 0.4;
+        this._environment._lanternObjs.forEach(lantern => {
+            gl.addIncludedOnlyMesh(lantern.mesh);
+        });
+        //webpack served from public       
+    }
+
+    private _showWin(): void {
+
+        //stop game sound and play end song
+        this.game.dispose();
+        this.end.play();
+        this._player.onRun.clear();
+
+        const winUI = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        winUI.idealHeight = 720;
+
+        const rect = new Rectangle();
+        rect.thickness = 0;
+        rect.background = "black";
+        rect.alpha = 0.4;
+        rect.width = 0.4;
+        winUI.addControl(rect);
+
+        const stackPanel = new StackPanel("credits");
+        stackPanel.width = 0.4;
+        stackPanel.fontFamily = "Viga";
+        stackPanel.fontSize = "16px";
+        stackPanel.color = "white";
+        winUI.addControl(stackPanel);
+
+        const wincreds = new TextBlock("special");
+        wincreds.resizeToFit = true;
+        wincreds.color = "white";
+        wincreds.text = "Special thanks to the Babylon Team!";
+        wincreds.textWrapping = true;
+        wincreds.height = "24px";
+        wincreds.width = "100%";
+        wincreds.fontFamily = "Viga";
+        stackPanel.addControl(wincreds);
+
+        //Credits for music & SFX
+        const music = new TextBlock("music", "Music");
+        music.fontSize = 22;
+        music.resizeToFit = true;
+        music.textWrapping = true;
+        
+        const source = new TextBlock("sources", "Sources: freesound.org, opengameart.org, and itch.io")
+        source.textWrapping = true;
+        source.resizeToFit = true;
+
+        const jumpCred = new TextBlock("jumpCred", "jump2 by LloydEvans09 - freesound.org");
+        jumpCred.textWrapping = true;
+        jumpCred.resizeToFit = true;
+
+        const walkCred = new TextBlock("walkCred", "Concrete 2 by MayaSama @mayasama.itch.io / ig: @mayaragandra");
+        walkCred.textWrapping = true;
+        walkCred.resizeToFit = true;
+
+        const gameCred = new TextBlock("gameSong", "Christmas synths by 3xBlast - opengameart.org"); 
+        gameCred.textWrapping = true;
+        gameCred.resizeToFit = true;
+
+        const pauseCred = new TextBlock("pauseSong", "Music by Matthew Pablo / www.matthewpablo.com - opengameart.org");
+        pauseCred.textWrapping = true;
+        pauseCred.resizeToFit = true;
+
+        const endCred = new TextBlock("startendSong", "copycat by syncopika - opengameart.org");
+        endCred.textWrapping = true;
+        endCred.resizeToFit = true;
+
+        const loseCred = new TextBlock("loseSong", "Eye of the Storm by Joth - opengameart.org");
+        loseCred.textWrapping = true;
+        loseCred.resizeToFit = true;
+
+        const fireworksSfx = new TextBlock("fireworks", "rubberduck - opengameart.org")
+        fireworksSfx.textWrapping = true;
+        fireworksSfx.resizeToFit = true;
+
+        const dashCred = new TextBlock("dashCred", "Woosh Noise 1 by potentjello - freesound.org");
+        dashCred.textWrapping = true;
+        dashCred.resizeToFit = true;
+
+        //quit, sparkwarning, reset
+        const sfxCred = new TextBlock("sfxCred", "200 Free SFX - https://kronbits.itch.io/freesfx");
+        sfxCred.textWrapping = true;
+        sfxCred.resizeToFit = true;
+
+        //lighting lantern, sparkreset
+        const sfxCred2 = new TextBlock("sfxCred2", "sound pack by wobbleboxx.com - opengameart.org");
+        sfxCred2.textWrapping = true;
+        sfxCred2.resizeToFit = true;
+
+        const selectionSfxCred = new TextBlock("select", "8bit menu select by Fupi - opengameart.org");
+        selectionSfxCred.textWrapping = true;
+        selectionSfxCred.resizeToFit = true;
+
+        stackPanel.addControl(music);
+        stackPanel.addControl(source);
+        stackPanel.addControl(jumpCred);
+        stackPanel.addControl(walkCred);
+        stackPanel.addControl(gameCred);
+        stackPanel.addControl(pauseCred);
+        stackPanel.addControl(endCred);
+        stackPanel.addControl(loseCred);
+        stackPanel.addControl(fireworksSfx);
+        stackPanel.addControl(dashCred);
+        stackPanel.addControl(sfxCred);
+        stackPanel.addControl(sfxCred2);
+        stackPanel.addControl(selectionSfxCred);
+
+        const mainMenu = Button.CreateSimpleButton("mainmenu", "RETURN");
+        mainMenu.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
+        mainMenu.fontFamily = "Viga";
+        mainMenu.width = 0.2
+        mainMenu.height = "40px";
+        mainMenu.color = "white";
+        winUI.addControl(mainMenu);
+
+        mainMenu.onPointerDownObservable.add(() => {
+            this._ui.transition = true;
+            this._ui.quitSfx.play();
+        })
+
+    }
+
+    private async _goToGame(): Promise<void> { 
+        //--SETUP SCENE--
+        this._scene.detachControl();
+        let scene = this._gamescene;
+
+        //--GUI--
+        const ui = new Hud(scene);
+        this._ui = ui;
+        //dont detect any inputs from this ui while the game is loading
+        scene.detachControl();
+
+        //IBL (image based lighting) - to give scene an ambient light
+        const envHdri = CubeTexture.CreateFromPrefilteredData("textures/envtext.env", scene);
+        envHdri.name = "env";
+        envHdri.gammaSpace = false;
+        scene.environmentTexture = envHdri;
+        scene.environmentIntensity = 0.04;
+
+        //--INPUT--
+        this._input = new PlayerInput(scene, this._ui); //detect keyboard/mobile inputs
+
+        //Initializes the game's loop
+        await this._initializeGameAsync(scene); //handles scene related updates & setting up meshes in scene
+
+        //--WHEN SCENE FINISHED LOADING--
+        await scene.whenReadyAsync();
+
+        //Actions to complete once the game loop is setup
+        scene.getMeshByName("outer").position = scene.getTransformNodeByName("startPosition").getAbsolutePosition(); //move the player to the start position
+        //set up the game timer and sparkler timer -- linked to the ui
+        this._ui.startTimer();
+        this._ui.startSparklerTimer(this._player.sparkler);
+        
+        //get rid of start scene, switch to gamescene and change states
+        this._scene.dispose();
+        this._state = State.GAME;
+        this._scene = scene;
+        this._engine.hideLoadingUI();
+        //the game is ready, attach control back
+        this._scene.attachControl();
+
+        //--SOUNDS--
+        this.game.play(); // play the gamesong
+    }
     
     private _createCanvas(): HTMLCanvasElement {
 
